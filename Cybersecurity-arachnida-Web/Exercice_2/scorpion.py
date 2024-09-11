b8bef2b6c6a6eb37d10243e5e9381bde8d094ed5
mochegri <mohamed.chegri@um6p.ma>
2024-09-11 23:19:50 +0100

    edit exo 2

--- Cybersecurity-arachnida-Web/Exercice_2/scorpion.py
+++ Cybersecurity-arachnida-Web/Exercice_2/scorpion.py
@@ -1,12 +1,34 @@
+#!/usr/bin/env python3
+
 from PIL import Image, ExifTags
+from sys import argv, exit
 
-img = Image.open('/Users/mohamedchegri/Desktop/2024-09-10 16.44.11.jpg')
+def extract_exif(image_path):
+    try:
+        img = Image.open(image_path)
+        exif_data = img._getexif()
+        if not exif_data:
+            return "No EXIF metadata found in the image."
+        exif = {ExifTags.TAGS.get(k, k): v for k, v in exif_data.items() if k in ExifTags.TAGS}
+        return exif
+    except Exception as e:
+        return f"Error processing the image: {e}"
 
-exif_data = img._getexif()
+def print_exif(exif_data):
+    if isinstance(exif_data, dict):
+        for tag, value in exif_data.items():
+            print(f"{tag}: {value}")
+    else:
+        print(exif_data)
 
-if exif_data:
-    exif = {ExifTags.TAGS.get(k, k): v for k, v in exif_data.items() if k in ExifTags.TAGS}
-    for tag, value in exif.items():
-        print(f"{tag}: {value}")
-else:
-    print("No EXIF metadata found in the image.")
+def main():
+    if len(argv) != 2:
+        print("Usage: python3 scorpion.py <image_path>")
+        exit(1)
+    
+    image_path = argv[1]
+    exif_data = extract_exif(image_path)
+    print_exif(exif_data)
+
+if __name__ == "__main__":
+    main()
